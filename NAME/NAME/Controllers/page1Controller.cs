5456d1f33b9ff3e669820f57c5dc5bcca01a8068
miniwei101 <miniwei101@yahoo.com>
2022-03-08 16:45:56 +0800

    test

---
+++ NAME/NAME/Controllers/page1Controller.cs
@@ -0,0 +1,64 @@
+﻿using NAME.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NAME.Controllers
+{
+    public class page1Controller : Controller
+    {
+        // GET: page1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult NameFirst()
+        {
+            //var now = new DateTime();
+            //int y = now.getFullYear();
+            //mh = (Today.getMonth() + 1);
+            //d = Today.getDate();
+            //int h = DateTime.Now.getHours();
+            //m = Today.getMinutes();
+            //s = Today.getSeconds();
+            ViewBag.KK = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            Save();
+            return View();
+        }
+
+        public int fId { get; set; }
+        public string fClass { get; set; }
+        public string fName { get; set; }
+
+        
+        public void Save()
+        {
+            Class1 x = new Class1();
+            //x.fName = Request.Form["txtName"];
+            ViewBag.KK = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            //ViewBag.KK2 = DateTime.Now.ToString("HH:mm:ss");
+            (new Class1()).create(x);  
+        
+        }
+        public ActionResult CheckIt()
+        {
+            string keyword = Request.Form["txtKeyword"];
+            string datetime = Request.Form["txtdatetime"];
+            //string noon = Request.Form["txtnoon"];
+            List<Class1> list = null;
+            if (string.IsNullOrEmpty(datetime) && string.IsNullOrEmpty(keyword))
+                list = (new Class1()).queryAll();
+            else if (!string.IsNullOrEmpty(keyword) && !string.IsNullOrEmpty(datetime))
+                list = (new Class1()).queryByKeyword(keyword);
+
+            //else if (noon=="before")
+              //  list = (new Class1()).queryByDate(datetime);
+            else
+                list = (new Class1()).queryByDate(datetime);
+            return View(list);
+        }
+    }
+    }
---
+++ NAME/NAME/Models/Class1.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace NAME.Models
+{
+    public class Class1
+    {
+        public int fId { get; set; }
+        public string fClass { get; set; }
+        public string fName { get; set; }
+        public string fTime { get; set; }
+        public int fnum { get; set; }
+        public List<Class1> queryBySql(string sql)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=.;Initial Catalog=shopping;Integrated Security=True";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Class1> list = new List<Class1>();
+            while (reader.Read())
+            {
+                Class1 x = new Class1()
+                {
+                    fId = (int)reader["id"],
+                    fnum = (int)reader["anum"],
+                    fClass = reader["aclass"].ToString(),
+                    fName = reader["aname"].ToString(),
+                    fTime = reader["atime"].ToString()
+                };
+                list.Add(x);
+            }
+            con.Close();
+            return list;
+        }
+        public void create(Class1 p)
+        {
+            string sql = "insert into CLASS_A(";
+            //sql += "fId,";
+            sql += "aclass,";
+            sql += "aname,";
+            sql += "atime,";
+            sql += "anum";
+            sql += ")VALUES(";
+            sql += "'" + "全端工程師就業養成班" + "',";
+            sql += "'" + "陳小饅" + "',";
+            sql += "'" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "',";
+            sql += "'" + "1" + "')";
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=.;Initial Catalog=shopping;Integrated Security=True";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public List<Class1> queryByKeyword(string keyword)
+        {
+            string sql = "select * from CLASS_A where aclass LIKE '%" + keyword + "%'";
+            sql += "or aname LIKE '%" + keyword + "%'";
+            sql += "or anum LIKE '%" + keyword + "%'";
+            return queryBySql(sql);
+        }
+        public List<Class1> queryByDate(string datetime)
+        {
+            //string noon = Request.Form["txtnoon"];
+            //int qq=
+            //string sql = "select * from CLASS_A where atime LIKE '%" + datetime + "%'";
+            string sql = "select * from CLASS_A where atime LIKE '%" + datetime + "%'";
+            return queryBySql(sql);
+        }
+        public List<Class1> queryAll()
+        {
+            return queryBySql("select * from CLASS_A");
+        }
+    }
+}
